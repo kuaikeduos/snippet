260e459ab921af84dc169c0a18a36e97ca036834
konglx <konglx90@gmail.com>
2020-10-04 22:02:02 +0800

    Create AnimateBubbles.js

---
+++ AnimateBubbles.js
@@ -0,0 +1,55 @@
+// https://codesandbox.io/s/reorder-elements-with-slide-transition-and-react-hooks-flip-211f2?file=/src/AnimateBubbles.js:0-1728
+
+// 列表位置变化时，发生冒泡动画
+
+import React, { useState, useLayoutEffect, useEffect } from "react";
+import usePrevious from "./hooks/usePrevious";
+import calculateBoundingBoxes from "./helpers/calculateBoundingBoxes";
+
+const AnimateBubbles = ({ children }) => {
+  const [boundingBox, setBoundingBox] = useState({});
+  const [prevBoundingBox, setPrevBoundingBox] = useState({});
+  const prevChildren = usePrevious(children);
+
+  useLayoutEffect(() => {
+    const newBoundingBox = calculateBoundingBoxes(children);
+    setBoundingBox(newBoundingBox);
+  }, [children]);
+
+  useLayoutEffect(() => {
+    const prevBoundingBox = calculateBoundingBoxes(prevChildren);
+    setPrevBoundingBox(prevBoundingBox);
+  }, [prevChildren]);
+
+  useEffect(() => {
+    const hasPrevBoundingBox = Object.keys(prevBoundingBox).length;
+
+    if (hasPrevBoundingBox) {
+      React.Children.forEach(children, child => {
+        const domNode = child.ref.current;
+        const firstBox = prevBoundingBox[child.key];
+        const lastBox = boundingBox[child.key];
+        const changeInX = firstBox.left - lastBox.left;
+
+        if (changeInX) {
+          requestAnimationFrame(() => {
+            // Before the DOM paints, invert child to old position
+            domNode.style.transform = `translateX(${changeInX}px)`;
+            domNode.style.transition = "transform 0s";
+
+            requestAnimationFrame(() => {
+              // After the previous frame, remove
+              // the transistion to play the animation
+              domNode.style.transform = "";
+              domNode.style.transition = "transform 500ms";
+            });
+          });
+        }
+      });
+    }
+  }, [boundingBox, prevBoundingBox, children]);
+
+  return children;
+};
+
+export default AnimateBubbles;
